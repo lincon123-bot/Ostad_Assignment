215a6840e39c2e52884a6332ddea2be3b37243b5
Test-1 <linconroy123@gmail.com>
2024-11-15 19:05:41 +0600

    New Update

---
+++ AppHomePage_Assignment_2.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyProfilePage(),
+    );
+  }
+}
+
+class MyProfilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Profile'),
+        backgroundColor: Colors.orange,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.settings),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.call),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ProfileItem(
+              icon: Icons.icecream,
+              text: 'Ice cream is very delicious right?',
+            ),
+            SizedBox(height: 20),
+            ProfileItem(
+              icon: Icons.code,
+              text: 'Programming is not boring if you love it',
+            ),
+            SizedBox(height: 20),
+            ProfileItem(
+              icon: Icons.egg,
+              text: 'If you submit code directly copy from chatgpt then mark will 0',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ProfileItem extends StatelessWidget {
+  final IconData icon;
+  final String text;
+
+  ProfileItem({required this.icon, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CircleAvatar(
+          radius: 50,
+          backgroundColor: Colors.purple[100],
+          child: Icon(
+            icon,
+            size: 50,
+            color: Colors.purple,
+          ),
+        ),
+        SizedBox(height: 10),
+        Text(
+          text,
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 16),
+        ),
+      ],
+    );
+  }
+}
